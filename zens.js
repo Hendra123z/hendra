87306b3a041e7b999be74f70c1ef582dc086fc78
Hendra123z <116165922+Hendra123z@users.noreply.github.com>
2022-10-19 18:39:29 +0700

    Update zens.js

--- zens.js
+++ zens.js
@@ -601,7 +601,7 @@ Ketik *nyerah* untuk menyerah dan mengakui kekalahan`
             }
             break
 	    case 'donasi': case 'sewabot': case 'sewa': case 'buypremium': case 'donate': {
-                zens.sendMessage(m.chat, { image: { url: 'https://youtube.com/channel/UC7flTES4TqWBLOPxk25_tsAfile/0b31865ef182a7681f9c3.jpg' }, caption: `*${ucapanWaktu} Kak ${m.pushName}*\n\n *Jika ingin berdonasi silahkan scan gambar diatas*\n\n*Jika ingin sewa bot atau premium*\n*Silahkan Chat Owner*\n\n*Atau klik link dibawah ini*\n_https://chat.whatsapp.com/Fj5z7EXvGLVARaZ0cJF8rd\n\n*Atau Transfer via*\n- *Gopay Dana Ovo Qris ShopeePay*\n chat nomor berikut : wa.me/6281227607864\n\n_Terima kasih_` }, { quoted: m })
+                zens.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/0b31865ef182a7681f9c3.jpg' }, caption: `*${ucapanWaktu} Kak ${m.pushName}*\n\n *Jika ingin berdonasi silahkan scan gambar diatas*\n\n*Jika ingin sewa bot atau premium*\n*Silahkan Chat Owner*\n\n*Atau premium*\n*Silahkan Chat Owner*\n\n*Atau klik link dibawah ini*\n_https://chat.whatsapp.com/Fj5z7EXvGLVARaZ0cJF8rd\n\n*Atau Transfer via*\n- *Gopay Dana Ovo Qris ShopeePay*\n chat nomor berikut : wa.me/6281227607864\n\n_Terima kasih_` }, { quoted: m })
             }
             break 
             case 'chat': {
